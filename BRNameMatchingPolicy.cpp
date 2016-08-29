7671b69b550994d0fc469944bf5bd7f072b9a378
codemiles <codemiles@gmail.com>
2016-08-29 10:03:21 -0700

    trying to fill a space-check log

--- BRNameMatchingPolicy.cpp
+++ BRNameMatchingPolicy.cpp
@@ -1,7 +1,5 @@
 /* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* vim: set ts=8 sts=2 et sw=2 tw=80: */
 /* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "BRNameMatchingPolicy.h"
@@ -16,9 +14,7 @@ BRNameMatchingPolicy::FallBackToCommonName(
   Time notBefore,
   /*out*/ FallBackToSearchWithinSubject& fallBackToCommonName)
 {
-  // (new Date("2015-08-23T00:00:00Z")).getTime() / 1000
-  static const Time AUGUST_23_2015 = TimeFromEpochInSeconds(1440288000);
-  // (new Date("2016-08-23T00:00:00Z")).getTime() / 1000
+
   static const Time AUGUST_23_2016 = TimeFromEpochInSeconds(1471910400);
   switch (mMode)
   {
